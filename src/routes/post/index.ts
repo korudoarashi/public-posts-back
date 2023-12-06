df1bdf6179c3a30b0cacc7f7361d8af62ebc3290
Pedro Duarte <pedro@uget.com.br>
2023-12-06 05:54:54 -0300

    [FEAT]: Created routes of post's CRUD - '/': Get all post's - '/:id': Get post by ID - '/create': Create a post using "title" and "content" - '/update': Update information of specific post (found by ID) - '/delete': Delete specific post by ID

--- src/routes/post/index.ts
+++ src/routes/post/index.ts
@@ -1,5 +1,85 @@
 import { Router } from 'express';
+import prismaClient from '../../databank';
 
 const router = Router();
+
+router.get('/', async (req, res) => {
+	const posts = await prismaClient.post.findMany({});
+	
+	res.status(200).send({ posts });
+});
+
+router.get('/:id', async (req, res) => {
+	const { id } = req.params;
+
+	const post = await prismaClient.post.findFirst({
+		where: {
+			id
+		}
+	});
+
+	if(!post)
+		return res.status(404).send({ post: null });
+
+	res.status(200).send({ post });
+});
+
+router.post('/create', async (req, res) => {
+	const {
+		title,
+		content
+	} = req.body;
+
+	const newPost = await prismaClient.post.create({
+		data: {
+			title,
+			content
+		}
+	});
+
+	res.status(200).send({ post: newPost });
+});
+
+router.put('/update', async (req, res) => {
+	const {
+		id,
+		title,
+		content
+	} = req.body;
+
+	if(!title && !content) 
+		return res.status(400).send({ updated: false });
+
+	const updatedPost = await prismaClient.post.updateMany({
+		where: {
+			id
+		}, 
+		data: {
+			title,
+			content
+		}
+	});
+
+	if(!updatedPost.count)
+		return res.status(404).send({ updated: false });
+	res.status(200).send({ updated: true });
+});
+
+router.delete('/delete', async (req, res) => {
+	const {
+		id
+	} = req.body;
+
+	const deletedPost = await prismaClient.post.deleteMany({
+		where: {
+			id
+		}
+	});
+
+	if(!deletedPost.count)
+		return res.status(404).send({ deleted: false });
+
+	res.status(200).send({ deleted: true });
+});
 
 export default router;
