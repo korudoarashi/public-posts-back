6819c3d3490b6cb1dca988628cc34be46662f625
Pedro Duarte <pedro@uget.com.br>
2023-12-07 06:05:24 -0300

    [FEAT]: Added cors to server

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 import express, { json } from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import postRouter from './routes/post';
 
 const app = express();
 
 /* Middlewares */
+
+app.use(cors());
 
 app.use(morgan('dev'));
 app.use(json());
